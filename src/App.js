34edf8d823e9449fccf0eaf924aa3ff01800c249
Carlos Vaquedano <chvb2002@hotmail.com>
2020-03-21 19:10:37 -0600

    consumo de api the clima

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from './components/Header'
+import Formulario from './components/Formulario'
 
 function App() {
+
+  const [busqueda, setBusqueda] = useState({
+    ciudad:'',
+    pais:''
+});
+
+const [consultar, setConsultar] = useState(false);
+
+const {ciudad, pais} = busqueda;
+
+useEffect(() => {
+
+  const consultarAPI = async () => {
+    const apikey='e40231c41a25ae56392b44252cf9eb10';
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${apikey}`;
+
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+    console.log(resultado)
+
+  };
+  if (consultar){
+    consultarAPI();
+    setConsultar(false);
+  }
+}, [ciudad, consultar, pais])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Fragment>
+     <Header
+      titulo="Clima React"
+     />
+     <div className="contenedor-form">
+       <div className="container">
+         <div className="row">
+           <div className="col m6 s12">
+             <Formulario
+              busqueda={busqueda}
+              setBusqueda={setBusqueda}
+              setConsultar={setConsultar}
+             />
+           </div>
+           <div className="col m6 s12">
+             2
+           </div>
+         </div>
+       </div>
+     </div>
+   </Fragment>
   );
 }
 
